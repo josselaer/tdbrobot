4f23932fafb380675aa3bd7ddc769501b257bd5e
josselaer <josselaer@smu.edu>
2016-11-06 16:03:37 -0600

    fixing globals

--- Adafruit-Motor-HAT-Python-Library-master/examples/tdb_bot.py
+++ Adafruit-Motor-HAT-Python-Library-master/examples/tdb_bot.py
@@ -18,11 +18,6 @@ class tdb_bot:
 	"""docstring for tdb_bot"""
 	curr_x = 0
 	curr_y = 0
-	
-	quarterTurnTime = 0.3
-	halfTurnTime = 0.44
-	oneFootTime = 0.92
-	robotSpeed = 200
 
 
 	def __init__(self, x, y):
@@ -31,6 +26,10 @@ class tdb_bot:
 		self.curr_x = x
 		self.curr_y = y
 		self.robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
+		self.quarterTurnTime = 0.3
+		self.halfTurnTime = 0.44
+		self.oneFootTime = 0.92
+		self.robotSpeed = 200
 
 		
 	def generate_ball(self):
@@ -48,28 +47,28 @@ class tdb_bot:
 		print(diff_y)
 		if(diff_x > 0):
 			print("turnng right")
-			self.robot.right(robotSpeed,quarterTurnTime)
+			self.robot.right(self.robotSpeed,self.quarterTurnTime)
 		elif(diff_x < 0):
 			print("left")
-			self.robot.left(robotSpeed,quarterTurnTime)
-		self.robot.forward(robotSpeed,(oneFootTime * abs(diff_x)))
+			self.robot.left(self.robotSpeed,self.quarterTurnTime)
+		self.robot.forward(self.robotSpeed,(self.oneFootTime * abs(diff_x)))
 		print("forward")
 		if(diff_x > 0):
-			self.robot.left(robotSpeed,quarterTurnTime)
+			self.robot.left(self.robotSpeed,self.quarterTurnTime)
 			print("straitening to left")
 		elif(diff_x < 0):
-			self.robot.right(robotSpeed,quarterTurnTime)
+			self.robot.right(self.robotSpeed,self.quarterTurnTime)
 			print("straitending to right")
 
 		if(diff_y < 0):
-			self.robot.right(robotSpeed,halfTurnTime)
+			self.robot.right(self.robotSpeed,self.halfTurnTime)
 			print("turn around")
 
-		self.robot.forward(robotSpeed,(oneFootTime*abs(diff_y)))
+		self.robot.forward(self.robotSpeed,(self.oneFootTime*abs(diff_y)))
 		print("froward again")
 
 		if(diff_y < 0):
-			self.robot.right(robotSpeed,halfTurnTime)
+			self.robot.right(self.robotSpeed,self.halfTurnTime)
 			print("straiten if backwards")
 
 		self.curr_x = ball[0]
